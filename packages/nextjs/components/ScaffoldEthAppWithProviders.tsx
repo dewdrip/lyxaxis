0ff148d769de82fb75bc8d05eac4d6ad64dcf62c
Valentine Orga <valentineorga@gmail.com>
2025-04-09 12:48:10 +0100

    chore: install chakra ui

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -6,11 +6,11 @@ import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowki
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { useTheme } from "next-themes";
-import { Toaster } from "react-hot-toast";
 import { WagmiProvider } from "wagmi";
 import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
+import { Provider as ChakraProvider } from "~~/components/ui/provider";
+import { Toaster } from "~~/components/ui/toaster";
 import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 
@@ -19,13 +19,15 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      <div className={`flex flex-col min-h-screen  font-space-grotesk`}>
-        <main className="relative flex flex-col w-[432px] mx-auto  flex-1 bg-base-300">
-          <Navbar /> {children}
-        </main>
-        <Footer />
-      </div>
-      <Toaster />
+      <ChakraProvider>
+        <div className={`flex flex-col min-h-screen  font-space-grotesk`}>
+          <main className="relative flex flex-col w-[432px] mx-auto  flex-1 bg-base-300">
+            <Navbar /> {children}
+          </main>
+          <Footer />
+        </div>
+        <Toaster />
+      </ChakraProvider>
     </>
   );
 };

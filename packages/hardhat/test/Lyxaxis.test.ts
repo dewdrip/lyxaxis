88b02ac67d09fbf7ab4d0596add1a3d77b9d0504
Valentine Orga <valentineorga@gmail.com>
2025-04-15 10:48:50 +0100

    test: signer management

--- packages/hardhat/test/Lyxaxis.test.ts
+++ packages/hardhat/test/Lyxaxis.test.ts
@@ -4,6 +4,8 @@ import { Lyxaxis } from "../typechain-types";
 import { MultiSigRegistry } from "../typechain-types";
 import { MultiSig } from "../typechain-types";
 import { LSP0ERC725Account } from "../typechain-types";
+import { ERC725YDataKeys } from "@lukso/lsp-smart-contracts";
+import { OPERATION_TYPES, PERMISSIONS } from "@lukso/lsp-smart-contracts";
 
 describe("Lyxaxis", function () {
   let lyxaxis: Lyxaxis;
@@ -83,6 +85,142 @@ describe("Lyxaxis", function () {
         lyxaxis,
         "Lyxaxis__NoOwners",
       );
+    });
+  });
+
+  describe("Signer Management", function () {
+    it("Should allow adding a new signer through UP", async function () {
+      // Create initial multisig
+      const name = "Test Wallet";
+      const chainId = 1;
+      const owners = [owner.address];
+      const signaturesRequired = 1;
+
+      const tx = await lyxaxis.createWallet(name, chainId, owners, signaturesRequired);
+      await tx.wait();
+
+      // Get the multisig address
+      const ownerMultisigs = await registry.getSignerMultisigs(owner.address);
+      const multisigAddress = ownerMultisigs[0];
+      const multisig = await ethers.getContractAt("MultiSig", multisigAddress);
+
+      // Get the universal profile
+      const universalProfileAddress = await multisig.universalProfile();
+      const universalProfile = await ethers.getContractAt("LSP0ERC725Account", universalProfileAddress);
+
+      // Set up LSP20 verification for the UP
+      const addressPermissionsKey =
+        ERC725YDataKeys.LSP6["AddressPermissions:Permissions"] + multisigAddress.substring(2);
+      const permissions = PERMISSIONS.CALL + PERMISSIONS.SUPER_CALL;
+
+      // Set permissions in ERC725Y storage
+      await multisig.executeTransaction(
+        universalProfileAddress,
+        0,
+        universalProfile.interface.encodeFunctionData("setData", [addressPermissionsKey, permissions]),
+        [],
+      );
+
+      // Add a new signer
+      const newSigner = addr2.address;
+      const newSignaturesRequired = 2;
+
+      const addSignerData = multisig.interface.encodeFunctionData("addSigner", [newSigner, newSignaturesRequired]);
+      await multisig.executeTransaction(
+        universalProfileAddress,
+        0,
+        universalProfile.interface.encodeFunctionData("execute", [
+          OPERATION_TYPES.CALL,
+          multisigAddress,
+          0,
+          addSignerData,
+        ]),
+        [],
+      );
+
+      // Verify new signer was added
+      expect(await multisig.isOwner(newSigner)).to.be.true;
+      expect(await multisig.signaturesRequired()).to.equal(newSignaturesRequired);
+    });
+
+    it("Should allow removing a signer through UP", async function () {
+      // Create initial multisig with two owners
+      const name = "Test Wallet";
+      const chainId = 1;
+      const owners = [owner.address, addr2.address];
+      const signaturesRequired = 2;
+
+      const tx = await lyxaxis.createWallet(name, chainId, owners, signaturesRequired);
+      await tx.wait();
+
+      // Get the multisig address
+      const ownerMultisigs = await registry.getSignerMultisigs(owner.address);
+      const multisigAddress = ownerMultisigs[0];
+      const multisig = await ethers.getContractAt("MultiSig", multisigAddress);
+
+      // Get the universal profile
+      const universalProfileAddress = await multisig.universalProfile();
+      const universalProfile = await ethers.getContractAt("LSP0ERC725Account", universalProfileAddress);
+
+      // Set up LSP20 verification for the UP
+      const addressPermissionsKey =
+        ERC725YDataKeys.LSP6["AddressPermissions:Permissions"] + multisigAddress.substring(2);
+      const permissions = PERMISSIONS.CALL + PERMISSIONS.SUPER_CALL;
+
+      // Set permissions in ERC725Y storage
+      await multisig.executeTransaction(
+        universalProfileAddress,
+        0,
+        universalProfile.interface.encodeFunctionData("setData", [addressPermissionsKey, permissions]),
+        [],
+      );
+
+      // Remove a signer
+      const signerToRemove = addr2.address;
+      const newSignaturesRequired = 1;
+
+      const removeSignerData = multisig.interface.encodeFunctionData("removeSigner", [
+        signerToRemove,
+        newSignaturesRequired,
+      ]);
+
+      await multisig.executeTransaction(
+        universalProfileAddress,
+        0,
+        universalProfile.interface.encodeFunctionData("execute", [
+          OPERATION_TYPES.CALL,
+          multisigAddress,
+          0,
+          removeSignerData,
+        ]),
+        [],
+      );
+
+      // Verify signer was removed
+      expect(await multisig.isOwner(signerToRemove)).to.be.false;
+      expect(await multisig.signaturesRequired()).to.equal(newSignaturesRequired);
+    });
+
+    it("Should revert if non-UP tries to add signer", async function () {
+      // Create initial multisig
+      const name = "Test Wallet";
+      const chainId = 1;
+      const owners = [owner.address];
+      const signaturesRequired = 1;
+
+      const tx = await lyxaxis.createWallet(name, chainId, owners, signaturesRequired);
+      await tx.wait();
+
+      // Get the multisig address
+      const ownerMultisigs = await registry.getSignerMultisigs(owner.address);
+      const multisigAddress = ownerMultisigs[0];
+      const multisig = await ethers.getContractAt("MultiSig", multisigAddress);
+
+      // Try to add signer directly (should fail)
+      const newSigner = addr2.address;
+      const newSignaturesRequired = 2;
+
+      await expect(multisig.addSigner(newSigner, newSignaturesRequired)).to.be.revertedWith("Not Universal Profile");
     });
   });
 });

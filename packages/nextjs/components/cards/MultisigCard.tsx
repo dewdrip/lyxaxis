44544ba3f5da7e8b314afc05eed77f91f80df03d
Valentine Orga <valentineorga@gmail.com>
2025-04-18 20:39:28 +0100

    refactor: display universal profile metadata

--- packages/nextjs/components/cards/MultisigCard.tsx
+++ packages/nextjs/components/cards/MultisigCard.tsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Address, Balance } from "../scaffold-eth";
+import Profile from "../Profile";
+import { Balance } from "../scaffold-eth";
 import { usePublicClient } from "wagmi";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
-import { useProfileMetadata } from "~~/hooks/useProfileMetadata";
 import MultiSigABI from "~~/utils/abis/MultiSigABI.json";
 
 export const MultisigCard = ({ multisigAddress }: { multisigAddress: string }) => {
   const publicClient = usePublicClient();
 
-  const [name, setName] = useState<string>();
   const [signaturesRequired, setSignaturesRequired] = useState<bigint>();
-  const [universalProfileAddress, setUniversalProfileAddress] = useState("");
+  const [universalProfileAddress, setUniversalProfileAddress] = useState<`0x${string}`>();
 
   const { data: owners } = useScaffoldReadContract({
     contractName: "MultiSigRegistry",
@@ -19,19 +18,12 @@ export const MultisigCard = ({ multisigAddress }: { multisigAddress: string }) =
     args: [multisigAddress],
   });
 
-  const { profile, fetchProfile } = useProfileMetadata();
-
   useEffect(() => {
     const getMultisigData = async () => {
       if (!publicClient || !multisigAddress) return;
 
       try {
-        const [nameResult, signaturesRequiredResult, universalProfileAddress] = await Promise.all([
-          publicClient.readContract({
-            address: multisigAddress,
-            abi: MultiSigABI,
-            functionName: "name",
-          }),
+        const [signaturesRequiredResult, universalProfileAddress] = await Promise.all([
           publicClient.readContract({
             address: multisigAddress,
             abi: MultiSigABI,
@@ -44,11 +36,8 @@ export const MultisigCard = ({ multisigAddress }: { multisigAddress: string }) =
           }),
         ]);
 
-        setName(nameResult as string);
         setSignaturesRequired(signaturesRequiredResult as bigint);
         setUniversalProfileAddress(universalProfileAddress as `0x${string}`);
-
-        await fetchProfile(universalProfileAddress as `0x${string}`);
       } catch (error) {
         console.error("Error fetching multisig data:", error);
       }
@@ -60,10 +49,7 @@ export const MultisigCard = ({ multisigAddress }: { multisigAddress: string }) =
   return (
     <Link href={`/multisig/${multisigAddress}`}>
       <div className="flex bg-base-100 text-center items-center justify-between min-w-[398px] py-4 px-4  rounded-xl">
-        <div className="flex flex-col items-start">
-          <p className="m-0">{name || "Unnamed Multisig"}</p>
-          {universalProfileAddress && <Address address={universalProfileAddress} />}
-        </div>
+        {universalProfileAddress && <Profile address={universalProfileAddress as `0x${string}`} />}
         <div>
           <p className="m-0">
             Signers: {signaturesRequired?.toString() || "?"}/{owners?.length || 0}

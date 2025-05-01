d3ba389cc315b60ffcc4e48bf5568081062a3d0d
Valentine Orga <valentineorga@gmail.com>
2025-05-01 14:03:44 +0100

    test: create multisig with default profile metadata

---
+++ packages/hardhat/test/utils/encodeProfileMetadata.ts
@@ -0,0 +1,41 @@
+import { ERC725 } from "@erc725/erc725.js";
+import LSP3ProfileMetadataSchemas from "@erc725/erc725.js/schemas/LSP3ProfileMetadata.json";
+import { BytesLike } from "ethers";
+import { ethers } from "hardhat";
+
+export interface ProfilePayload {
+  name: string;
+  description: string;
+  tags: string[];
+  links: any[];
+  profileImage?: any[];
+  backgroundImage?: any[];
+}
+
+export const encodeProfileMetadata = async (_profile: ProfilePayload): Promise<BytesLike> => {
+  let profileMetadata = {
+    LSP3Profile: _profile,
+  };
+
+  const profileMetadataHash = ethers.keccak256(ethers.toUtf8Bytes(JSON.stringify(profileMetadata)));
+
+  const lsp3DataValue = {
+    verification: {
+      method: "keccak256(utf8)",
+      data: profileMetadataHash,
+    },
+    // this is an IPFS CID of a LSP3 Profile Metadata example, you can use your own
+    url: `ipfs://`,
+  };
+
+  const erc725 = new ERC725(LSP3ProfileMetadataSchemas);
+
+  const encodedData = erc725.encodeData([
+    {
+      keyName: "LSP3Profile",
+      value: lsp3DataValue,
+    },
+  ]);
+
+  return encodedData.values[0] as BytesLike;
+};
